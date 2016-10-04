dd690973e8eef20c03c2b02e1dbb725db444f53b
LinkinYoung <1143052146@qq.com>
2016-10-04 22:38:36 +0800

    完成登陆API并提供示例前端文件

---
+++ API/signIn.php
@@ -0,0 +1,26 @@
+<?php
+    include_once('config.php');
+    unset($_SESSION['studentID']);
+
+    try {
+        $dbh = new PDO("mysql:host={$db_config['host']};dbname={$db_config['dbName']}", $db_config['user'], $db_config['pwd'], [PDO::ATTR_PERSISTENT => true, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"]);
+    } catch (PDOExveption $e) {
+        print('{"result":"Database Fatal"');
+        die();
+    }
+
+    $stmt = $dbh->prepare("SELECT studentID, pwd FROM `SingerInfo` WHERE studentID = ?");
+    $stmt->execute(array($_POST['studentID']));
+    $row = $stmt->fetch(PDO::FETCH_NAMED);
+
+    if ($row == null) {
+        print('{"result":"NewUser"}');
+        $_SESSION['studentID'] = $_POST['studentID'];
+    } else if ($row['pwd'] == $_POST['pwd']) {
+        print('{"result":"Succeeded"}');
+        $_SESSION['studentID'] = $_POST['studentID'];
+    } else {
+        print('{"result":"Failed"}');
+        unset($_SESSION['studentID']);
+    }
+?>
