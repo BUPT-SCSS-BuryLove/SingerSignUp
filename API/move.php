e3bc7b7be0a95a8562bc9124e09b69d86da5c58d
LinkinYoung <1143052146@qq.com>
2016-10-05 00:57:37 +0800

    增加文件整理脚本

--- API/move.php
+++ API/move.php
@@ -7,4 +7,20 @@
         print('{"result":"Database Fatal"');
         die();
     }
+
+    mkdir('../FILE/本部');
+    foreach($dbh->query('SELECT location, school, name, file FROM SingerInfo WHERE location = "校本部"') as $row) {
+        copy($row['file'], '../FILE/本部/'.$row['location'].'-'.$row['school'].'-'.$row['name'].'-'.basename($row['file']));
+    }
+    print("本部文件收集完成\n");
+    mkdir('../FILE/沙河');
+    foreach($dbh->query('SELECT location, school, name, file FROM SingerInfo WHERE location = "沙河校区"') as $row) {
+        copy($row['file'], '../FILE/沙河/'.$row['location'].'-'.$row['school'].'-'.$row['name'].'-'.basename($row['file']));
+    }
+    print("沙河文件收集完成\n");
+    mkdir('../FILE/宏福');
+    foreach($dbh->query('SELECT location, school, name, file FROM SingerInfo WHERE location = "宏福校区"') as $row) {
+        copy($row['file'], '../FILE/宏福/'.$row['location'].'-'.$row['school'].'-'.$row['name'].'-'.basename($row['file']));
+    }
+    print("宏富文件收集完成\n");
 ?>
