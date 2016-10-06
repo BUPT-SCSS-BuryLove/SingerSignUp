67b282a1236683c2510029cde6511e2e25bcbcfa
carol233 <jie322007@163.com>
2016-10-07 00:37:40 +0800

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -1,5 +1,6 @@
 
 $(document).ready(function(){
+	
 	$('#confirm_id').click(function(e){//注册
 		$.ajax({
 			type:"POST",
@@ -26,26 +27,28 @@ $(document).ready(function(){
 	$("#submit_btn").click(function(e){//除文件外的其他信息
 
 		var url = "API/info.php"; // the script where you handle the form input.
-		 
+		
+		var sub = new Object;
+		sub.studentID = $("#buptid").val();
+		sub.pwd = hex_md5($("#password").val());
+		sub.campus = $("#campus").val();
+		sub.school = $("#school").val();
+		sub.name = $("#name").val();
+		sub.gender = $("#gender").val();
+		sub.contact = $("#contact").val();
+		sub.college_class = $("#class").val();
+		sub.title = $("#song").val();
+		sub.noMusic = $("#way").val();
+		sub.type = $("input[name='single_or_group'][checked]").val();
+		sub.teamName = $("#team_name").val();
+		sub.teamPeople = $("#number").val();
+		sub.teamInfo = $("#others").val();
+		
 		$.ajax({
 			type: "POST",
 			url: url,
-			data: {
-				studentID = $("#buptid").val();
-				pwd = hex_md5($("#password").val());
-				campus = $("#campus").val();
-				school = $("#school").val();
-				name = $("#name").val();
-				gender = $("#gender").val();
-				contact = $("#contact").val();
-				college_class = $("#class").val();
-				title = $("#song").val();
-				noMusic = $("#way").val();
-				type = $("input[name='single_or_group'][checked]").val();
-				teamName = $("#team_name").val();
-				teamPeople = $("#number").val();
-				teamInfo = $("#others").val();
-			},
+			dataType:"json",
+			data:sub,
 			success: function(data){
 				if(data.result == "Succeeded"){
 					Materialize.toast("提交成功！", 6000);
